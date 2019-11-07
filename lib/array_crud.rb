b9dbd678c15139f7f7fba8e6a4bce3368bf55fe6
Sean Good <tree662@protonmail.com>
2019-11-07 01:30:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my array = [num1, num2, num3, num4]
+  my array = [element1, element2, element3, element4]
 end
 
 def add_element_to_end_of_array(array, element)
