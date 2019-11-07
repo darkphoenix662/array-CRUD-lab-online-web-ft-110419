54341ce9ea4272a8ec12fcdc7969f815730eb5b6
Sean Good <tree662@protonmail.com>
2019-11-07 01:31:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my array = [element1, element2, element3, element4]
+  my array = ["ketchup", "mustard", "mayonnaise", "hp"]
 end
 
 def add_element_to_end_of_array(array, element)
